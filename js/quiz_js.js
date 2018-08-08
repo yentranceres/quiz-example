470460142d4bee015e246676cccbea24a1433460
Yen Tran <yen.tran@ceresolutions.com>
2018-08-08 17:58:53 +0700

    edit button

--- js/quiz_js.js
+++ js/quiz_js.js
@@ -1,60 +1,48 @@
 $(document).ready(function () {
     var userInputs = [];
+    console.log(userInputs);
     var missAnswer = [];
     console.log(missAnswer);
 
-    //Show question
     $.getJSON('js/data.json', function (data) {
-        getQuestion(data);
+        getQuestion(data); //show question html
 
-        //Click option to select answer
-        $(document).on('click', '.option', function () {
-            var questionID = $(this).parents('.quiz').data('id');
-            var answerID = $(this).attr('value');
-            addUserInput(questionID, answerID, data);
-        });
 
-        // When user click "Next" and "Previous"
+        // When user click "Next" and "Previous" --> next question
         $(document).on('click', '.next-button', function () {
-            var id = $('.quiz:visible').data('id');
-            var nextId = $('.quiz:visible').data('id') + 1;
-            var missQuestionId = $(this).parents('.content-body').find('.active').data('id');
+            var currentId = $('.quiz:visible').data('id');
+            var questionNumber = $(this).parents('.content-body').find('.active').data('id');
+            var answerNumber = $("input[name='optionsradio" + currentId + "']:checked").val();
 
-            //Question has not had answer
-            missingQA(missQuestionId, userInputs,missAnswer);
+            buttonPrevious();
 
-            //Switch question
-            $('[data-id="' + id + '"]').hide().removeClass('active');
-            $('[data-id="' + nextId + '"]').show().addClass('active');
-            if ($('.pre-button:hidden').length === 1) {
-                $('.pre-button').show();
-            }
-            if (nextId === 5) {
-                $('.next-button').hide();
-                $('.done-button').show();
-            }
+            checkAnswer(questionNumber, answerNumber, currentId, data);
+
         });
 
         $(document).on('click', '.pre-button', function () {
-            var id = $('.quiz:visible').data('id');
-            var preId = $('.quiz:visible').data('id') - 1;
-            $('[data-id="' + id + '"]').hide().removeClass('active');
-            $('[data-id="' + preId + '"]').show().addClass('active');
-            $('.done-button').hide();
-            $('.next-button').show();
-
-            if (preId === 1) {
-                $('.pre-button').hide();
-            }
+            buttonNext();
         });
 
-        //submit answer
         $(document).on('click', '.done-button', function (data) {
-            var missQuestionId = $(this).parents('.content-body').find('.active').data('id');
-            missingQA(missQuestionId, userInputs,missAnswer);
+            var currentId = $('.quiz:visible').data('id');
+            var firstIndex = missAnswer[0];
+            var questionNumber = $(this).parents('.content-body').find('.active').data('id');
+            var answerNumber = $("input[name='optionsradio" + currentId + "']:checked").val();
+
+            checkAnswer(questionNumber, answerNumber, currentId, data);
+            $('[data-id="' + firstIndex + '"]').show();
+            $('[data-id="' + currentId + '"]').hide();
+
+            buttonPrevious();
+            buttonNext();
+
             compareArray(userInputs, data);
         });
     });
+
+
+    <!-- all Functions -->
 
     function getQuestion(allAnswers) {
         var nId;
@@ -74,10 +62,44 @@ $(document).ready(function () {
         }
     }
 
-    function addUserInput(question, answer, allAnswers) {
+    function buttonPrevious() {
+        var currentId = $('.quiz:visible').data('id');
+        var nextId = $('.quiz:visible').data('id') + 1;
+        var preId = $('.quiz:visible').data('id') - 1;
+        console.log(preId);
+        console.log(nextId);
+
+        $('[data-id="' + currentId + '"]').hide().removeClass('active');
+        $('[data-id="' + nextId + '"]').show().addClass('active');
+        if ($('.pre-button:hidden').length == 1) {
+            $('.pre-button').show();
+        }
+        if (nextId == 5) {
+            $('.next-button').hide();
+            $('.done-button').show();
+        }
+    }
+
+    function buttonNext() {
+        var currentId = $('.quiz:visible').data('id');
+        var preId = $('.quiz:visible').data('id') - 1;
+
+        $('[data-id="' + currentId + '"]').hide().removeClass('active');
+        $('[data-id="' + preId + '"]').show().addClass('active');
+        $('.done-button').hide();
+        $('.next-button').show();
+
+        if (preId == 1) {
+            $('.pre-button').hide();
+        }
+    }
+
+
+    function checkAnswer(question, answer, id, allAnswers) {
         var obj = {
             question: question,
-            answer: answer
+            answer: answer,
+            id: id
         };
 
         //check question has been stored ?????
@@ -93,53 +115,34 @@ $(document).ready(function () {
             }
         }
 
-        if (index !== null) { //update
-            userInputs[index] = obj;
+        if (index == null) { //update
+            if (obj.answer == undefined) {
+                missAnswer.push(obj.id);
+            } else {
+                userInputs.push(obj);
+            }
         } else { //create
-            userInputs.push(obj);
+            userInputs[index] = obj;
         }
     }
 
-    console.log(userInputs);
+    function compareArray(userInputs, allAnswers) {
+        var score = 0;
+        for (var x = 0; x < userInputs.length; x++) {
+            for (var k = 0; k < allAnswers.length; k++) {
 
-    //Question has not had answer
-    function missingQA(question, userInputs, missAnswer) {
-        var missQuestion = {question: question};
-        if (){
-            missAnswer.push(missQuestion);
-        }
-
-        if (missAnswer){
-            for (y = 0; y <= missAnswer.length; y++){
-                for (t = 0; t <= userInputs.length; t++){
-                    if (missAnswer[y]== userInputs[t]){
-                        missAnswer.slice(y,1);
+                if (userInputs[x].question == allAnswers[k].id) {
+                    if (userInputs[x].answer == allAnswers[k].correct) {
+                        score = score + 1;
+                    }
+                    else {
+                        score = score;
                     }
                 }
             }
         }
+        $('#result').append('Your score is ' + score);
+        $('.content-body').hide();
     }
 
-
-        //caculate score
-        function compareArray(userInputs ,allAnswers, missAnswer) {
-            var score = 0;
-            for (var x = 0; x < userInputs.length; x++) {
-                for (var k = 0; k < allAnswers.length; k++) {
-
-                    if (userInputs[x].question == allAnswers[k].id) {
-                        if (userInputs[x].answer == allAnswers[k].correct) {
-                            score = score + 1;
-                        }
-                        else {
-                            score = score;
-                        }
-                    }
-                }
-            }
-            $('#result').append('Your score is ' + score);
-            $('.content-body').hide();
-        }
 });
-
-
